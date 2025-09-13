f423e89e3f7cfec9948a866e13cc1f59a0c3afaa
B-Spatial <larsenbrandont@gmail.com>
2025-09-12 23:18:59 -0700

    E: split of main.py file funtions.

--- main.py
+++ main.py
@@ -1,16 +1,8 @@
-def get_book_text(path_to_file):
-    with open(path_to_file) as f:
-        file_contents = f.read()
-    return file_contents
-
-def count_words(file_contents):
-    words = file_contents.split()
-    num_words = len(words)
-    print(f"{num_words} words found in the document")
-
+from stats import *
 
 def main():
     file_path = "/Users/brandon/github/B-Spatial/bookbot/books/frankenstein.txt"
+
     print(get_book_text(file_path))
     count_words(get_book_text(file_path))
 
---
+++ stats.py
@@ -0,0 +1,9 @@
+def get_book_text(path_to_file):
+    with open(path_to_file) as f:
+        file_contents = f.read()
+    return file_contents
+
+def count_words(file_contents):
+    words = file_contents.split()
+    num_words = len(words)
+    print(f"{num_words} words found in the document")
