35bf94472f29b3b3915d845c58ccbc01a45d7764
B-Spatial <larsenbrandont@gmail.com>
2025-09-15 16:47:19 -0700

    H: Import sys module added.

--- main.py
+++ main.py
@@ -1,27 +1,33 @@
 from stats import *
+import sys
 
 def main():
-    file_path = "/Users/brandon/github/B-Spatial/bookbot/books/frankenstein.txt"
 
-    text = get_book_text(file_path)
-    #print(text)
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    
+    else:
+        path = sys.argv[1]
+        text = get_book_text(path)
+
+
 
     num_words = count_words(text)
-    char_count = (count_char(text))
+    #char_count = (count_char(text))
     sorted_dict = (sort_dict(count_char(text)))
 
     print("============ BOOKBOT ============")
-    print("Analyzing book found at books/frankenstein.txt...")
+    print(f"Analyzing book found at {path} ...")
     print("----------- Word Count ----------")
     print(f"Found {num_words} total words")
     print("--------- Character Count -------")
-    #print(char_count)
-    #print(sorted_dict)
+    
     for item in sorted_dict:
         if not item["char"].isalpha(): continue
         
         else:
-            print (f"{item["char"]}: {item["num"]}")
+            print (f"{item['char']}: {item['num']}")
 
 
 main()
